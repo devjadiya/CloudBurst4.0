16f34768c1017f4c47b6d2ecea629bbf7c673352
sarthak-io <sarthak25ic049@satiengg.in>
2023-11-01 19:21:21 +0530

    20.0

--- example/urls.py
+++ example/urls.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from home import views
 urlpatterns = [
-   path('',views.home,name='home'),
-   # path('acadmics',views.acadmics,name='acadmics'),
-   # path('code',views.code,name='code'),
-   # path('clubs',views.clubs,name='clubs'),
-   # path('happenings',views.happenings,name='happenings'),
-   # path('projects',views.projects,name='projects'),
-   # path('contribution',views.contribution,name='contribution'),
-   # path('developers',views.developers,name='developers')
+   path('index',views.home,name='home'),
+   path('acadmics',views.acadmics,name='acadmics'),
+   path('code',views.code,name='code'),
+   path('clubs',views.clubs,name='clubs'),
+   path('happenings',views.happenings,name='happenings'),
+   path('projects',views.projects,name='projects'),
+   path('contribution',views.contribution,name='contribution'),
+   path('developers',views.developers,name='developers')
  
 
 ]
