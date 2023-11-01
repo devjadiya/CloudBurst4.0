f6fc840ee326f21dc7c5321fe2fc2e1b115020e7
sarthak-io <sarthak25ic049@satiengg.in>
2023-11-01 20:41:37 +0530

    25.0

--- home/views.py
+++ home/views.py
@@ -2,17 +2,17 @@ from django.shortcuts import render,HttpResponse
 
 def home(request):
     return render(request,"index.html")
-def acadmics(request):
-    return render(request,"acadmics.html")
-def code(request):
-    return render(request,"code.html")
-def clubs(request):
-    return render(request,"clubs.html")
-def happenings(request):
-    return render(request,"happenings.html")
-def projects(request):
-    return render(request,"projects.html")
-def contribution(request):
-    return render(request,"contribution.html")
-def developers(request):
-    return render(request,"developers.html")
+# def acadmics(request):
+#     return render(request,"acadmics.html")
+# def code(request):
+#     return render(request,"code.html")
+# def clubs(request):
+#     return render(request,"clubs.html")
+# def happenings(request):
+#     return render(request,"happenings.html")
+# def projects(request):
+#     return render(request,"projects.html")
+# def contribution(request):
+#     return render(request,"contribution.html")
+# def developers(request):
+#     return render(request,"developers.html")
