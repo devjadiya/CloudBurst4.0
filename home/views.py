34605fe3a8da3b20aae6d0866247df65f8a5c16a
sarthak-io <sarthak25ic049@satiengg.in>
2023-11-01 20:47:46 +0530

    25.0

--- home/views.py
+++ home/views.py
@@ -1,18 +1,13 @@
 from django.shortcuts import render,HttpResponse
 
 def home(request):
-    return render(request,"index.html")
-# def acadmics(request):
-#     return render(request,"acadmics.html")
-# def code(request):
-#     return render(request,"code.html")
-# def clubs(request):
-#     return render(request,"clubs.html")
-# def happenings(request):
-#     return render(request,"happenings.html")
-# def projects(request):
-#     return render(request,"projects.html")
-# def contribution(request):
-#     return render(request,"contribution.html")
-# def developers(request):
-#     return render(request,"developers.html")
+    if request.method == 'POST':
+        my_input_value = request.POST.get('my_input', 'Default Value')
+    else:
+        my_input_value = 'Default Value'
+    
+    context = {
+        'variable': my_input_value
+    }
+    return render(request, 'index.html', context)
+
