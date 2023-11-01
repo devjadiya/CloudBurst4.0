1f39b7b24c64b1ba1421d197aa78c8ebc1942b69
sarthak-io <sarthak25ic049@satiengg.in>
2023-11-01 18:27:26 +0530

    17.0

--- example/settings.py
+++ example/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'example.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'example', 'build')],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,12 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'example','build/static')
+    os.path.join(BASE_DIR,"static"),
 ]
